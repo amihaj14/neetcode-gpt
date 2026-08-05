a8882c45f4edfdc3257f4d37f38e9e9fdd965594
Alexander Mihajlovski <alexandermih2005@gmail.com>
2026-08-05 16:42:50 -0400

    Implement Self-attention head (Attention & Transformers 1/3)

---
+++ model/attention.py
@@ -0,0 +1,28 @@
+import torch
+import torch.nn as nn
+from torchtyping import TensorType
+
+class SingleHeadAttention(nn.Module):
+
+    def __init__(self, embedding_dim: int, attention_dim: int):
+        super().__init__()
+        torch.manual_seed(0)
+        self.key = nn.Linear(embedding_dim, attention_dim, bias=False)
+        self.query = nn.Linear(embedding_dim, attention_dim, bias=False)
+        self.value = nn.Linear(embedding_dim, attention_dim, bias=False)
+
+    def forward(self, embedded: TensorType[float]) -> TensorType[float]:
+        k = self.key(embedded)
+        q = self.query(embedded)
+        v = self.value(embedded)
+
+        scores = q @ torch.transpose(k,1,2)
+        context_len, attention_dim = k.shape[1], k.shape[2]
+        scores = scores / (attention_dim ** 0.5)
+
+        lower_triangular = torch.tril(torch.ones(context_len, context_len))
+        mask = lower_triangular == 0
+        scores = scores.masked_fill(mask, float('-inf'))
+        scores = nn.functional.softmax(scores, dim=2)
+
+        return torch.round(scores @ v, decimals=4)
